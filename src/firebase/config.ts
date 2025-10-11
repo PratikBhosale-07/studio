35b0d5d4cd41f6fb758114b4612ce77a18a36ea1
Pratik Bhosale <pratikbhosale914@gmail.com>
2025-10-11 13:27:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-619134478-17180",
+  "appId": "1:715317515559:web:e37937323d8ec2261b0502",
+  "apiKey": "AIzaSyCq0-gg4F7qnIlAULZYcP_bKnH3N9gCR4Q",
+  "authDomain": "studio-619134478-17180.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "715317515559"
+};
